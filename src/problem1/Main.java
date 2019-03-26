7bac5c03080ac7cd7debcdfeb2a636f73e6d987c
Pavel <pavel.leo.nangfack-tiokeng@ocsbstudent.ca>
2019-03-26 09:54:35 -0400

    minecraft, im learning lol

--- src/problem1/Main.java
+++ src/problem1/Main.java
@@ -5,6 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		// Read a value. Print out from 1 to the input
 		Scanner in=new Scanner(System.in);
+		
+		System.out.println("Give me a number");
+		int x=in.nextInt();
+		int y=1;
+		
+		while (y <= x) {
+			System.out.println(y);
+			y = y + 1;
+		}
+		
 	}
 
 }
--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -4,8 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Read in two numbers. Add up all the values between them
+		//take the first number and the second number, add 1 to the first number, and add that number to the total.
+		//for every time that x is not equal to y you keep looping but dont print a lin
 		Scanner in=new Scanner(System.in);
-
+		
+		System.out.println("What is the first number?");
+		int x = in.nextInt();
+		System.out.println("What is the second number?");
+		int y = in.nextInt();
+		int total = 0;
+		
+		for (int ctr=x;ctr<=y;ctr++) {
+			total=total+ctr;
+		}
+		
+		
+		System.out.println(total);
 	}
 
 }
