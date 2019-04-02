f25316f86b7f87e1b2298dcf909a8b4faebd6cb2
Pavel <pavel.leo.nangfack-tiokeng@ocsbstudent.ca>
2019-04-02 10:47:16 -0400

    finished #3

--- src/problem3/Main.java
+++ src/problem3/Main.java
@@ -4,7 +4,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Read in a word and print out one character per String
-		Scanner in=new Scanner(System.in);
+		// 
+		Scanner sc=new Scanner(System.in);
+		
+		
+		
+		System.out.println("Type a word");
+		String word = sc.nextLine();
+		int l = word.length();
+		int letter = 0;
+			
+		
+		while (l > 0) {
+			char w = word.charAt(letter);
+			
+			System.out.println(w);
+			
+			l = l - 1;
+			letter = letter + 1;
+		}
+			
+			
+		
 	}
 
 }
--- src/problem4/Main.java
+++ src/problem4/Main.java
@@ -6,9 +6,40 @@ public class Main {
 	public static void main(String[] args) {
 		// Ask the user 10 multiplication questions. Check their answers and
 		//give them their score
+		//get 2 random numbers from 1 to 10 and display them as a multiplication question
+		//get the answer of those numbers and set it as a variable
 		Random rnd=new Random();
 		Scanner in=new Scanner(System.in);
-
+		int questions = 0;
+		int ans1 = 0;
+		int ans2 = 0;
+		int points = 0;
+		
+		
+		System.out.println("Here are your questions");
+		
+		while (questions<10) {
+			int num1 = rnd.nextInt(10-0+1)+0;
+			int num2 = rnd.nextInt(10-0+1)+0;
+			System.out.println(num1 + " x " + num2);
+			ans1 = in.nextInt();
+			ans2 = num1 * num2;
+					if (ans1 == ans2) {
+						System.out.println("Correct!"
+								+ " "
+								+ "");
+						points = points + 1;
+					} else {
+						System.out.println("Wrong!"
+								+ " "
+								+ "");
+					}
+			
+			questions = questions + 1;
+		}
+		
+		System.out.println("You got a score of " + points + "/10");
+		
 	}
 
 }
